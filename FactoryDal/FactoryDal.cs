31d763bc98e215d67a9170a1af7407b6e33487de
trivediashish06051981 <trivediashish.spamrs@gmail.com>
2016-10-14 21:38:11 +0530

    Add files via upload

---
+++ FactoryDal/FactoryDal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+using InterfaceCustomer;
+using AdoDotnetDAL;
+using CommonDAL;
+using InterfaceDal;
+using EfDal;
+namespace FactoryDal
+{
+    public static class FactoryDalLayer<AnyType> // Design pattern :- Simple factory pattern
+    {
+        private static IUnityContainer ObjectsofOurProjects = null;
+
+
+        public static AnyType Create(string Type)
+        {
+            // Design pattern :- Lazy loading. Eager loading
+            if (ObjectsofOurProjects == null)
+            {
+                ObjectsofOurProjects = new UnityContainer();
+               
+                ObjectsofOurProjects.RegisterType<IRepository<CustomerBase>,
+                    CustomerDAL>("ADODal");
+                ObjectsofOurProjects.RegisterType<IRepository<CustomerBase>,
+                    EfDalAbstract<CustomerBase>>("EFDal");
+                ObjectsofOurProjects.RegisterType<IUow,AdoUow>("AdoUOW");
+                ObjectsofOurProjects.RegisterType<IUow, EUow>("EfUOW");
+              
+            }
+            //Design pattern :-  RIP Replace If with Poly
+            return ObjectsofOurProjects.Resolve<AnyType>(Type) ;
+        }
+    }
+}
