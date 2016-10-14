be6e2938249e410cb6c3698b92ffa3b6cb8ff086
trivediashish06051981 <trivediashish.spamrs@gmail.com>
2016-10-14 21:36:48 +0530

    Add files via upload

---
+++ CustomerProjectPhase1/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterfaceCustomer;
+namespace MiddleLayer
+{
+   
+    public class Customer : CustomerBase
+    {
+        
+        public Customer(IValidation<ICustomer> obj, string _CustType) : base(obj)
+        {
+            CustomerType = _CustType;
+        }
+        
+    }
+   
+}
