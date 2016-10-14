0615e7b567eb9f1fd7f71e9aec713a65687949f4
trivediashish06051981 <trivediashish.spamrs@gmail.com>
2016-10-14 21:36:27 +0530

    Add files via upload

---
+++ CommonDAL/CommonDal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterfaceDal;
+namespace CommonDAL
+{
+    public abstract class AbstractDal<AnyType> : IRepository<AnyType>
+    {
+       
+       protected List<AnyType> AnyTypes = new List<AnyType>();
+
+        public virtual void Add(AnyType obj)
+        {
+            foreach (AnyType temp in AnyTypes)
+            {
+                if (obj.Equals(temp))
+                {
+                    return;
+                }
+            }
+            AnyTypes.Add(obj);
+        }
+
+        public virtual void Update(AnyType obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IEnumerable<AnyType> Search()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SetUnitWork(IUow uow)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<AnyType> GetData()
+        {
+            return AnyTypes;
+        }
+
+
+        public virtual AnyType GetData(int Index)
+        {
+            return AnyTypes[Index];
+        }
+
+
+        public virtual void Cancel(int Index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
