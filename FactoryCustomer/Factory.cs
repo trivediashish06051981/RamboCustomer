c87082cc6356a329b049cb29bcc6fd1915c76c41
trivediashish06051981 <trivediashish.spamrs@gmail.com>
2016-10-14 21:37:36 +0530

    Add files via upload

---
+++ FactoryCustomer/Factory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiddleLayer;
+using Microsoft.Practices.Unity;
+using InterfaceCustomer;
+using ValidationAlgorithms;
+using InterfaceDal;
+namespace FactoryCustomer
+{
+    public static class Factory<AnyType> // Design pattern :- Simple factory pattern
+    {
+        private static IUnityContainer ObjectsofOurProjects = null;
+            
+        
+        public static AnyType Create(string Type)
+        {
+            // Design pattern :- Lazy loading. Eager loading
+            if (ObjectsofOurProjects == null)
+            {
+                ObjectsofOurProjects = new UnityContainer();
+
+                IValidation<ICustomer> custValidation = new PhoneValidation(
+                                                        new CustomerBasicValidation());
+                ObjectsofOurProjects.RegisterType<CustomerBase, Customer>
+                                    ("Lead"
+                                    , new InjectionConstructor(
+                                        custValidation,"Lead"));
+                custValidation = new CustomerBasicValidation();
+                ObjectsofOurProjects.RegisterType<CustomerBase, Customer>
+                                    ("SelfService"
+                                    , new InjectionConstructor(
+                                        custValidation, "SelfService"));
+                custValidation = new CustomerAddressValidation(
+                                    new CustomerBasicValidation());
+                ObjectsofOurProjects.RegisterType<CustomerBase, Customer>
+                                    ("HomeDelivery"
+                                    , new InjectionConstructor(
+                                        custValidation, "HomeDelivery"));
+                custValidation = new PhoneValidation(
+                                  new CustomerBillValidation(
+                                  new CustomerAddressValidation(
+                                    new CustomerBasicValidation())));
+                ObjectsofOurProjects.RegisterType<CustomerBase, Customer>
+                                    ("Customer"
+                                    , new InjectionConstructor(
+                                        custValidation, "Customer"));
+                
+                
+               
+            }
+            //Design pattern :-  RIP Replace If with Poly
+            return ObjectsofOurProjects.Resolve<AnyType>(Type); 
+        }
+    }
+}
