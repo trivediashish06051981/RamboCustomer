76d5db777c960ef8a1bc800d50edb8a6c361d940
trivediashish06051981 <trivediashish.spamrs@gmail.com>
2016-10-14 21:37:10 +0530

    Add files via upload

---
+++ EfDal/EfDal.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterfaceDal;
+using System.Data.Entity;
+using InterfaceCustomer;
+using System.Data.Entity.Infrastructure;
+
+namespace EfDal
+{
+    // Design pattern :- Adpater pattern ( class Adapter pattern)
+    public class EfDalAbstract<AnyType> :  IRepository<AnyType>
+        where AnyType : class
+    {
+        DbContext dbcont = null;
+        public EfDalAbstract()
+        {
+            dbcont = new EUow(); // Self contained transaction
+        }
+        public void Add(AnyType obj)
+        {
+            dbcont.Set<AnyType>().Add(obj); // in memory committ
+        }
+        
+        public void Save()
+        {
+            dbcont.SaveChanges(); //physical committ
+        }
+        public void Update(AnyType obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AnyType> Search()
+        {
+            return dbcont.Set<AnyType>().
+                     AsQueryable<AnyType>().
+                         ToList<AnyType>();       
+        }
+
+
+
+        public void SetUnitWork(IUow uow)
+        {
+            dbcont = ((EUow)uow); // Global transaction UOW
+        }
+
+
+        public IEnumerable<AnyType> GetData()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public AnyType GetData(int Index)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Cancel(int Index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+   
+
+
+    public class EUow : DbContext, IUow
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustomerBase>()
+                       .ToTable("tblCustomer");
+        }
+        public EUow() : base("name=Conn")
+        {
+
+        }
+        public void Committ()
+        {
+            SaveChanges();
+        }
+
+        public void RollBack() // Adapter
+        {
+            Dispose();
+        }
+    }
+}
